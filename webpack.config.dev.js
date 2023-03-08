6c0d5a462871191dfa57d336db5b8573d89f74f3
DAVID MERCADO JUANES <dmjuanes_@abc.gob.bo>
2023-03-07 23:56:25 -0400

    MODIFICACION DE DEPENDENCIAS PARA USO DE DEVsERVER

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,7 +12,7 @@ module.exports = {
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   mode: 'development',
-  watch: true,
+  // watch: true,
   resolve: {
     extensions: ['.js'],
     alias: {
@@ -76,5 +76,11 @@ module.exports = {
       ]
     }),
     new Dotenv(),
-  ]
+  ],
+  devServer: {
+    // contentBase:path.join(__dirname, 'dist'),
+    compress:true,
+    historyApiFallback:true,
+    port:3007,
+}
 }
